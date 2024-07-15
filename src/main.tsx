398dcc273d45637f346c14c3eb22212c1689a92e
JaeAnHan <jaean1999@naver.com>
2024-07-15 17:53:51 +0900

    feat: combine css files into one css file
    - delete App.css
    - rename index.css to main.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '@/index.css';
-import '@/App.css';
+import '@/main.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
-// import { NavigateContextProvider } from '@/context/navigateContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/*<NavigateContextProvider>*/}
     <RouterProvider router={router} />
-    {/*</NavigateContextProvider>*/}
   </React.StrictMode>,
 );
